53c370d6c1ac52503dbbd427a1ef6f9d3fae347d
art-of-michelangelo <omarx816@hotmail.com>
2021-12-09 22:32:23 +0300

    update 27

--- app.py
+++ app.py
@@ -2,15 +2,23 @@ import json, config
 from flask import Flask, request, jsonify, render_template
 from binance.client import Client
 from binance.enums import *
-
+from telethon import TelegramClient, events, sync
 
 app = Flask(__name__)
 
 client = Client(config.API_KEY, config.API_SECRET)
 
+api_id = config.WEBHOOK_PASSPHRASE
+api_hash = config.WEBHOOK_PASSPHRASE
+client = TelegramClient('anon', api_id, api_hash)
 
+@client.on(events.NewMessage(chats='Signals Global Channel'))
+async def my_event_handler(event):
+    print(event.raw_text) 
 
-
+client.start()
+client.run_until_disconnected()
+ 
 def order(side, quantity, symbol, order_type=ORDER_TYPE_MARKET):
     try:
         print(f"sending order {order_type} - {side} {quantity} {symbol}")

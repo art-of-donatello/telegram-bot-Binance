0f9470a98819aac2723d8695705bee72284027cf
art-of-michelangelo <omarx816@hotmail.com>
2021-12-09 22:40:57 +0300

    ad

--- app.py
+++ app.py
@@ -8,8 +8,8 @@ app = Flask(__name__)
 
 client = Client(config.API_KEY, config.API_SECRET)
 
-api_id = config.WEBHOOK_PASSPHRASE
-api_hash = config.WEBHOOK_PASSPHRASE
+api_id = config.API_ID
+api_hash = config.API_HASH
 client = TelegramClient('anon', api_id, api_hash)
 
 @client.on(events.NewMessage(chats='Signals Global Channel'))
@@ -19,56 +19,3 @@ async def my_event_handler(event):
 client.start()
 client.run_until_disconnected()
  
-def order(side, quantity, symbol, order_type=ORDER_TYPE_MARKET):
-    try:
-        print(f"sending order {order_type} - {side} {quantity} {symbol}")
-        params={'quoteOrderQty':50,}
-        balance = client.get_asset_balance(asset='USDT')
-        
-        amount = None
-        price = None
-        #order = client.create_order(symbol=symbol, side=side, type=order_type,quoteOrderQty=50)
-        order = client.create_order(symbol=symbol, side=side, type=order_type, quantity=quantity)
-    except Exception as e:
-        print("an exception occured - {}".format(e))
-        return False
-
-    return order
-	
-
-
-		
-@app.route('/')
-def welcome():
-    return render_template('index.html')
-
-@app.route('/webhook', methods=['POST'])
-def webhook():
-    #print(request.data)
-    data = json.loads(request.data)
-    
-    if data['passphrase'] != config.WEBHOOK_PASSPHRASE:
-        return {
-            "code": "error",
-            "message": "Nice try, invalid passphrase"
-        }
-
-    side = data['strategy']['order_action'].upper()
-    quantity = data['strategy']['order_contracts']
-    parite=data['ticker']
-    print(parite)
-	#orderplace("DOGEUSD",5)
-    order_response = order(side, quantity, parite)
-
-    if order_response:
-        return {
-            "code": "success",
-            "message": "order executed"
-        }
-    else:
-        print("order failed")
-
-        return {
-            "code": "error",
-            "message": "order failed"
-        }

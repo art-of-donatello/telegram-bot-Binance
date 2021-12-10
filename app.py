aab1a8d27ddf837c34e30d759a59f5d2536f5fb4
art-of-michelangelo <omarx816@hotmail.com>
2021-12-10 04:20:30 +0300

    Update app.py

--- app.py
+++ app.py
@@ -13,7 +13,7 @@ from binance.helpers import round_step_size
 from telethon import TelegramClient, events, sync
 
 # In[33]:
-quit()
+
 
 client = Client(config.API_KEY, config.API_SECRET)
 
@@ -99,7 +99,7 @@ async def my_event_handler(event):
 
 #client.start()
 print("Started")
-client.run_until_disconnected()
+#client.run_until_disconnected()
      
 
 
